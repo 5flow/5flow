86c0e179862f1470cdc765b30e0a227dc4b1f870
Aaditya-Dev-9 <adityasanap1122@gmail.com>
2026-07-27 18:23:24 +0530

    blog_fix

--- lib/cms/blog.ts
+++ lib/cms/blog.ts
@@ -30,7 +30,7 @@ type WpCategory = {
 
 type AcfImageField = string | { url?: string; sizes?: Record<string, string> } | undefined;
 
-const EXCLUDED_BLOG_CATEGORY_SLUGS = ['case-studies', 'downloads', 'download'];
+const EXCLUDED_BLOG_CATEGORY_SLUGS = ['case-studies', 'downloads', 'download', 'webinars', 'webinar'];
 
 function formatDate(dateString: string): string {
   try {
