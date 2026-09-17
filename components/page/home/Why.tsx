44d2e88aaab4d3b2bd50cc43334af7022ebf2eed
Aaditya-Dev-9 <adityasanap1122@gmail.com>
2026-09-17 13:17:56 +0530

    home page de

--- components/page/home/Why.tsx
+++ components/page/home/Why.tsx
@@ -95,7 +95,7 @@ const Why = ({
                 alt="QC Assist screen showing AI-assisted packaging quality review"
                 fill
                 sizes="(min-width: 1024px) 50vw, 100vw"
-                className="object-cover object-top"
+                className="object-contain object-center"
               />
             </div>
           </div>
